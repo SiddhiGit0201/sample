0f37820186665bfc3172af57c6f5aac99c1f2ae6
siddhika <siddhika.asati@gmail.com>
2022-06-04 17:57:24 +0530

    First commit

---
+++ Helloword/src/studyeasy/Helloword.java
@@ -0,0 +1,11 @@
+package studyeasy;
+
+public class Helloword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Siddhika");
+
+	}
+
+}
